1ebad70e6b270df14fc0d67bdd92dd9a4fd4298a
mahdi <feyzollahi.m@gmail.com>
2019-02-05 19:19:33 +0330

    second

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,31 +4,71 @@ import org.json.simple.parser.ParseException;
 
 import java.io.*;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class Main {
+    public static void print(Object object){
+        java.lang.System.out.println(object);
+    }
     public static void main(String[] args) throws IOException {
+        System system = new System();
+        String commandline;
+        ArrayList<String> commands = new ArrayList<String>();
         URL url = Main.class.getResource("UserJson.txt");
         java.lang.System.out.println(url.getPath());
-        File f = new File(url.getPath());
+        String filename="UserJson.txt";
+        String cwd = java.lang.System.getProperty("user.dir");
+        print(cwd);
+        Path pathToFile = Paths.get(filename);
+        print(pathToFile.toAbsolutePath());
+        commands = (ArrayList<String>) Files.readAllLines(Paths.get("src/main/resources/UserJson.txt"));
+        Main.print(commands);
+        for(String command:  commands){
+            Main.doCommand(command);
+        }
+
+
         java.lang.System.out.println("Working Directory = " +
                 java.lang.System.getProperty("user.dir"));
-        BufferedReader b = null;
-        try {
-            b = new BufferedReader(new FileReader(f));
-        } catch (FileNotFoundException e) {
-            java.lang.System.out.println("s");
-            e.printStackTrace();
-        }
-        String line = b.readLine();
+
         JSONParser parser = new JSONParser();
         JSONObject jsonObject = new JSONObject();
-        try {
-            jsonObject = (JSONObject) parser.parse(line);
-        } catch (ParseException e) {
-            java.lang.System.out.println("json cannot be parsed");
-            java.lang.System.out.println(line);
-            e.printStackTrace();
+//        try {
+//            jsonObject = (JSONObject) parser.parse(line);
+//        } catch (ParseException e) {
+//            java.lang.System.out.println("json cannot be parsed");
+//            java.lang.System.out.println(line);
+//            e.printStackTrace();
+//        }
+//        User user1 = new User(jsonObject);
+    }
+    private static void doCommand(String command){
+        String commandOrder = command.split(" ", 2)[0];
+        String jsonInfo = command.split(" ", 2)[1];
+        if(commandOrder.equals("register")){
+            registerCommand(jsonInfo);
         }
-        User user1 = new User(jsonObject);
+        else if(commandOrder.equals("addProject")){
+            addProjectCommand(jsonInfo);
+        }
+        else if(commandOrder.equals("bid")){
+//            bidCommand(jsonInfo);
+        }
+        else if(commandOrder.equals("auction")){
+//            auctionCommand(jsonInfo);
+        }
+
+//        if(commandOrder)
+    }
+
+    private static void addProjectCommand(String jsonInfo) {
+    }
+    private static void registerCommand(String jsonInfo){
+        JSONObject jsonObject = new JSONObject();
+//        JSONParser.
+//        User user
     }
 }
--- src/main/java/System.java
+++ src/main/java/System.java
@@ -1,7 +1,86 @@
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 import java.util.ArrayList;
 
 public class System {
     private ArrayList<User> users;
     private ArrayList<Project> projects;
     private ArrayList<Bid> bids;
+    private ArrayList<String> commands;
+
+    public ArrayList<String> getCommands() {
+        return commands;
+    }
+
+    public void setCommands(ArrayList<String> commands) {
+        this.commands = commands;
+    }
+
+    public void doCommands() {
+        for (String command : commands) {
+            String commandOrder = command.split(" ", 2)[0];
+            String jsonInfo = command.split(" ", 2)[1];
+            if (commandOrder.equals("register")) {
+                registerCommand(jsonInfo);
+            } else if (commandOrder.equals("addProject")) {
+//                addProjectCommand(jsonInfo);
+            } else if (commandOrder.equals("bid")) {
+//            bidCommand(jsonInfo);
+            } else if (commandOrder.equals("auction")) {
+//            auctionCommand(jsonInfo);
+            }
+        }
+    }
+
+    public void registerCommand(String jsonInfo) {
+        JSONParser parser = new JSONParser();
+        JSONObject jsonObject = null;
+        try {
+            jsonObject = (JSONObject) parser.parse(jsonInfo);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            Main.print("jsonInfo cannot be parsed");
+        }
+        User user = new User(jsonObject);
+        this.addUser(user);
+    }
+
+    public ArrayList<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(ArrayList<User> users) {
+        this.users = users;
+    }
+
+    public void addProject(Project project) {
+        this.projects.add(project);
+    }
+
+    public void addUser(User user) {
+        this.users.add(user);
+    }
+
+    public void addBid(Bid bid) {
+        this.bids.add(bid);
+    }
+
+    public ArrayList<Project> getProjects() {
+        return projects;
+    }
+
+    public void setProjects(ArrayList<Project> projects) {
+        this.projects = projects;
+    }
+
+    public ArrayList<Bid> getBids() {
+        return bids;
+    }
+
+    public void setBids(ArrayList<Bid> bids) {
+        this.bids = bids;
+    }
 }
